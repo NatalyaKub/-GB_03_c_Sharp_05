c479f5b8876161c4f083c2a113e2963c5571c223
Natalyae <ngk.s@mail.ru>
2023-02-25 02:07:31 +0100

    Add code hw02

--- hw_02/Program.cs
+++ hw_02/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] ArraySet (int length){
+    int index = 0;
+    int[] array = new int[length];
+    Console.WriteLine("Задаем массив");
+    while (index < length){
+        array[index] = new Random().Next(0,100);
+        index++;
+    }
+    Console.WriteLine();
+    return array;
+}
+
+void ArrayPrint(int[] col){
+    int l = col.Length;
+    int index = 0;
+    Console.Write("[ ");
+    while (index < l){
+        Console.Write($"{col[index]}, ");
+        index++;
+    }
+    //Console.Write($"|array[array.length-1]|");
+    Console.Write("]");
+    Console.WriteLine();
+
+}
+
+int QuantityEvenNumbers(int length, int[] array){
+    int sum = 0;
+    int index = 0;
+    while (index < length){
+        if (index % 2 != 0){
+            sum = sum + array[index];
+        }
+        index++;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Задайте размер массива:");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = ArraySet(length);
+
+ArrayPrint(array);
+//QuantityEvenNumbers();
+
+int sumArray = QuantityEvenNumbers(length, array);
+Console.WriteLine($"Сумма элементов на нечетных позициях: {sumArray}");
